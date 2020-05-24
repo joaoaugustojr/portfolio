1dd5119752b556054fdddc57b4e33836faa6a84b
João Augusto <jhonaugustjunior@gmail.com>
2020-05-23 22:51:52 -0300

    config sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,8 @@ module.exports = {
         name: `Portfólio - João Augusto`,
         short_name: `portfolio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#545454`,
+        theme_color: `#545454`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
@@ -29,54 +29,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-nprogress`,
       options: {
-        // Setting a color is optional.
         color: `red`,
-        // Disable the loading spinner.
-        showSpinner: true,
+        showSpinner: false,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     // The property ID; the tracking code won't be generated without it
-    //     trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
-    //     // Defines where to place the tracking script - `true` in the head and `false` in the body
-    //     head: false,
-    //     // Setting this parameter is optional
-    //     anonymize: true,
-    //     // Setting this parameter is also optional
-    //     respectDNT: true,
-    //     // Avoids sending pageview hits from custom paths
-    //     exclude: ["/preview/**", "/do-not-track/me/too/"],
-    //     // Delays sending pageview hits on route update (in milliseconds)
-    //     pageTransitionDelay: 0,
-    //     // Enables Google Optimize using your container Id
-    //     optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-    //     // Enables Google Optimize Experiment ID
-    //     experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-    //     // Set Variation ID. 0 for original 1,2,3....
-    //     variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-    //     // Defers execution of google analytics script after page load
-    //     defer: false,
-    //     // Any additional optional fields
-    //     sampleRate: 5,
-    //     siteSpeedSampleRate: 10,
-    //     cookieDomain: "example.com",
-    //   },
-    // },
-    `gatsby-plugin-sass`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-offline`,
-    // {
-    //   resolve: `gatsby-plugin-prefetch-google-fonts`,
-    //   options: {
-    //     fonts: [
-    //       {
-    //         family: `Roboto`,
-    //         subsets: [`300`, `500`, `700`],
-    //       },
-    //     ],
-    //   },
-    // }
   ]
 }
